19545f1709c68912a15729620b50a95a874ffb45
Bagas Try Ambodo <bagasambodo@gmail.com>
2017-02-07 19:54:43 +0700

    commit SQL SERVER 2

--- asp_mvc_2/Controllers/HomeController.cs
+++ asp_mvc_2/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using asp_mvc_2.Security;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,6 +15,15 @@ namespace asp_mvc_2.Controllers
         }
         [Authorize]
         public ActionResult Welcome()
+        {
+            return View();
+        }
+        [AuthorizeRoles("Admin")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+        public ActionResult UnAuthorized()
         {
             return View();
         }
---
+++ asp_mvc_2/Security/AuthorizeRoleAttribute.cs
@@ -0,0 +1,40 @@
+﻿using asp_mvc_2.Models.DB;
+using asp_mvc_2.Models.EntityManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace asp_mvc_2.Security
+{
+    public class AuthorizeRolesAttribute : AuthorizeAttribute
+    {
+        private readonly string[] userAssignedRoles;
+        public AuthorizeRolesAttribute(params string[] roles)
+        {
+            this.userAssignedRoles = roles;
+        }
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            bool authorize = false;
+            using (DemoDBEntities db = new DemoDBEntities())
+            {
+                UserManager UM = new UserManager();
+                foreach (var roles in userAssignedRoles)
+                {
+                    authorize = UM.IsUserInRole(httpContext.User.Identity.Name,
+                    roles);
+                    if (authorize)
+                        return authorize;
+                }
+            }
+            return authorize;
+        }
+        protected override void HandleUnauthorizedRequest(AuthorizationContext
+        filterContext)
+        {
+            filterContext.Result = new RedirectResult("~/Home/UnAuthorized");
+        }
+    }
+}
